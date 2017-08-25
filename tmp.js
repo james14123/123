6d51f94039ea314c6561ad2edbb450ae42ad091f
james14123 <james14123@hotmail.com.tw>
2017-08-25 14:16:12 +0800

    e

--- tmp.js
+++
@@ -1,8 +0,0 @@
-var texture_from_file = new BABYLON.Texture("dress.jpg", scene);
-        
-var pbr = new BABYLON.PBRSpecularGlossinessMaterial("pbr", scene);
-
-for (var i = 0; i < scene.meshes.length; i++){
-            scene.meshes[i].material.diffuseTexture = texture_from_file;
-
-            }
