35c3af0ec92cf24aedef4a5f18af9ce793bfb075
james14123 <james14123@hotmail.com.tw>
2017-08-29 15:04:02 +0800

    123

--- models/mdcloth/TOB_mdcloth.d.ts
+++ models/mdcloth/TOB_mdcloth.d.ts
@@ -11,9 +11,29 @@ declare module TOB_mdcloth{
     export function matReadAhead(materialsRootDir) : void;
     export function defineMaterials(scene : BABYLON.Scene, materialsRootDir : string = "./") : void;
 
-    class mdcloth extends BABYLON.Mesh {
-        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : mdcloth);
+    class cloth1weight129 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight129);
     }
+
+    class cloth1weight107 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight107);
+    }
+
+    class cloth1weight87 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight87);
+    }
+
+    class cloth1weight67 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight67);
+    }
+
+    class cloth1weight47 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight47);
+    }
+
+    export function defineCameras(scene : BABYLON.Scene, positionOffset? : BABYLON.Vector3) : void;
+
+    export function defineLights(scene : BABYLON.Scene, positionOffset? : BABYLON.Vector3) : void;
 
     export function freshenShadowRenderLists(scene : BABYLON.Scene) : void;
 }
