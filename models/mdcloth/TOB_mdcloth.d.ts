d923390cc218ce5fa990e1065765fe4b3c131e16
james14123 <james14123@hotmail.com.tw>
2017-08-31 16:18:08 +0800

    32

--- models/mdcloth/TOB_mdcloth.d.ts
+++ models/mdcloth/TOB_mdcloth.d.ts
@@ -11,24 +11,104 @@ declare module TOB_mdcloth{
     export function matReadAhead(materialsRootDir) : void;
     export function defineMaterials(scene : BABYLON.Scene, materialsRootDir : string = "./") : void;
 
-    class cloth1weight129 extends BABYLON.Mesh {
-        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight129);
+    class cloth1weight67height206 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight67height206);
     }
 
-    class cloth1weight107 extends BABYLON.Mesh {
-        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight107);
+    class cloth1weight67height172 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight67height172);
     }
 
-    class cloth1weight87 extends BABYLON.Mesh {
-        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight87);
+    class cloth1weight127height189 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight127height189);
     }
 
-    class cloth1weight67 extends BABYLON.Mesh {
-        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight67);
+    class cloth1weight127height172 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight127height172);
     }
 
-    class cloth1weight47 extends BABYLON.Mesh {
-        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight47);
+    class cloth1weight127height206 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight127height206);
+    }
+
+    class cloth1weight127height155 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight127height155);
+    }
+
+    class cloth1weight127height138 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight127height138);
+    }
+
+    class cloth1weight107height189 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight107height189);
+    }
+
+    class cloth1weight107height172 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight107height172);
+    }
+
+    class cloth1weight107height155 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight107height155);
+    }
+
+    class cloth1weight107height138 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight107height138);
+    }
+
+    class cloth1weight87height172 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight87height172);
+    }
+
+    class cloth1weight87height189 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight87height189);
+    }
+
+    class cloth1weight87height155 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight87height155);
+    }
+
+    class cloth1weight87height138 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight87height138);
+    }
+
+    class cloth1weight67height189 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight67height189);
+    }
+
+    class cloth1weight67height155 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight67height155);
+    }
+
+    class cloth1weight67height138 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight67height138);
+    }
+
+    class cloth1weight47height189 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight47height189);
+    }
+
+    class cloth1weight47height172 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight47height172);
+    }
+
+    class cloth1weight47height155 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight47height155);
+    }
+
+    class cloth1weight47height138 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight47height138);
+    }
+
+    class cloth1weight107height206 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight107height206);
+    }
+
+    class cloth1weight87height206 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight87height206);
+    }
+
+    class cloth1weight47height206 extends BABYLON.Mesh {
+        constructor(name: string, scene: BABYLON.Scene, materialsRootDir: string = "./", source? : cloth1weight47height206);
     }
 
     export function defineCameras(scene : BABYLON.Scene, positionOffset? : BABYLON.Vector3) : void;
